6cb6fb3c31ecd6d66114ba1fce0e939e3252fd94
Micaelaalomia <2008168@newton.pe>
2022-04-29 10:54:59 -0500

    Commit 29/04

--- src/main/java/org/example/Plant.java
+++ src/main/java/org/example/Plant.java
@@ -1,19 +1,26 @@
 package org.example;
 
+import javafx.scene.paint.Color;
+
 public class Plant {
 
     String name;
     int number;
     String type;
     String location;
+    double x; double y; double r; Color color;
+    String notes;
 
-    public Plant(String name, int number, String type, String location) {
+    public Plant(String name, int number, String type, String location, double x, double y, double r, Color color) {
         this.name = name;
         this.number = number;
         this.type = type;
         this.location = location;
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.color = color;
     }
 
-    String notes;
 }
 
